149017c3a3abd988e0d6e2a35b4b1b02bb886fd2
dayblas <dayblas007@gmail.com>
2025-04-17 01:44:10 -0300

    id

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 
-const ItemDetail = ({nombre, precio, img, descripcion}) => {
+const ItemDetail = ({id, nombre, precio, img, descripcion, stock}) => {
+
+  const [agregarCantidad, setAgregarCantidad] = useState(0)
+
+  const manejadorCantidad= (cantidad) => {
+    setAgregarCantidad(cantidad)
+    console.log("productos agregados:" + cantidad)
+  }
     
   return (
     <div>
@@ -9,6 +17,10 @@ const ItemDetail = ({nombre, precio, img, descripcion}) => {
         <img src={img} alt={nombre}/>
         <p>Descripcion:{descripcion}</p>
         
+
+        {
+          agregarCantidad > 0 ?( <Link to="/cart"> Terminar Compra</Link> ) : ( <ItemCount inicial={1} stock={stock} funcionAgregar={manejadorCantidad}/> )
+        }
         
     </div>
   )
